5d32935b252d933951420f06ecc946bbf695daff
Pakkapon <karnwayn0019@gmail.com>
2024-09-09 22:42:29 +0700

    revise title

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React test test test</h1>
+      <h1>Example deploy react with firebase</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
